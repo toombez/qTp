88f8edc6402559a993923f69be5778fb1ddb9e87
toombez <timurtokaev.toombez@gmail.com>
2022-02-17 01:27:32 +0300

    Add constructor to AbstractComponent
    Also fix width naming from previous commit

--- src/struct/AbstractComponent.ts
+++ src/struct/AbstractComponent.ts
@@ -1,6 +1,6 @@
 export default abstract class AbstractComponent {
-    private width: number = 1;
-    private height: number = 1;
+    constructor(private width: number = 1, 
+                private height: number = 1) {}
 
     public get Height() {
         return this.height;
@@ -9,7 +9,7 @@ export default abstract class AbstractComponent {
         if (value <= 0) throw new RangeError('The value is less than or equal to 0');
         this.height = value;
     }
-    public get Widht() {
+    public get Width() {
         return this.width;
     }
     public set Width(value: number) {
