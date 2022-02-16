8d1d42669ddc43db8c58e8ffa81b9ba5b3599208
toombez <timurtokaev.toombez@gmail.com>
2022-02-17 02:01:17 +0300

    Add components to AbstractComponent class
    Add Point type for setting child components location  inside
    parent component

--- src/struct/AbstractComponent.ts
+++ src/struct/AbstractComponent.ts
@@ -1,3 +1,5 @@
+import { Point } from './types';
+
 export default abstract class AbstractComponent {
     constructor(private width: number = 1, 
                 private height: number = 1) {}
@@ -15,5 +17,20 @@ export default abstract class AbstractComponent {
     public set Width(value: number) {
         if (value <= 0) throw new RangeError('The value is less than or equal to 0');
         this.width = value;
+    }
+
+    /** TODO (fix)
+     *  Component now can overlap and override each other
+     */
+    public components = new Map<Point, AbstractComponent>();
+    public addComponent(component: AbstractComponent, point: Point): this {
+        const xComponentOverflow: boolean = point.x + component.Width > this.width;
+        const yComponentOverflow: boolean = point.y + component.Height > this.height;
+
+        if (xComponentOverflow || yComponentOverflow) 
+        throw RangeError('Child component bigger than parent');
+
+        this.components.set(point, component);
+        return this;
     }
 }
