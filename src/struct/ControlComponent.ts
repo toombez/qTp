63d686af28963dceed16408d225ac70afd7d5bd8
toombez <timurtokaev.toombez@gmail.com>
2022-02-17 22:42:37 +0300

    Add activeComponent static field

--- src/struct/ControlComponent.ts
+++ src/struct/ControlComponent.ts
@@ -2,6 +2,14 @@ import AbstractComponent from "./AbstractComponent";
 import { ExecuteControl } from './types';
 
 export default class ControlComponent extends AbstractComponent {
+    private static activeComponent: ControlComponent;
+    public static get ActiveComponent () {
+        return this.activeComponent;
+    }
+    public static set ActiveComponent (component) {
+        this.activeComponent = component;
+    }
+
     public controls = new Map<string, ExecuteControl>();
     public addControl(sequince: string, execute: ExecuteControl): this {
         this.controls.set(sequince, execute);
