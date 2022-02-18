5055a232000cc41a3d8475291214edbe02f77009
toombez <timurtokaev.toombez@gmail.com>
2022-02-18 20:42:23 +0300

    Remove components from AbstractComponent class
    Some components can contain only text, which self cannot be component.

--- src/struct/AbstractComponent.ts
+++ src/struct/AbstractComponent.ts
@@ -1,5 +1,3 @@
-import { Point } from './types';
-
 export default abstract class AbstractComponent {
     constructor(private width: number = 1, 
                 private height: number = 1) {}
@@ -17,20 +15,5 @@ export default abstract class AbstractComponent {
     public set Width(value: number) {
         if (value <= 0) throw new RangeError('The value is less than or equal to 0');
         this.width = value;
-    }
-
-    /** TODO (fix)
-     *  Component now can overlap and override each other
-     */
-    public components = new Map<Point, AbstractComponent>();
-    public addComponent(component: AbstractComponent, point: Point): this {
-        const xComponentOverflow: boolean = point.x + component.Width > this.width;
-        const yComponentOverflow: boolean = point.y + component.Height > this.height;
-
-        if (xComponentOverflow || yComponentOverflow) 
-        throw new RangeError('Child component bigger than parent');
-
-        this.components.set(point, component);
-        return this;
     }
 }
