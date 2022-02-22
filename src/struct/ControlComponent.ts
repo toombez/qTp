bcdf60cb4d3f5e098e8842bf928a490b05a1f822
toombez <timurtokaev.toombez@gmail.com>
2022-02-22 13:28:13 +0300

    Refactor ControlComponent class
    ControlComponent class now extends GroupComponent.
    Remove global controls field

--- src/struct/ControlComponent.ts
+++ src/struct/ControlComponent.ts
@@ -1,34 +1,19 @@
-import AbstractComponent from "./AbstractComponent";
-import { ExecuteControl } from './types';
+import GroupComponent from "./GroupComponent";
+import { ExecuteControl } from "./types";
 
-export default class ControlComponent extends AbstractComponent {
-    private static activeComponent: ControlComponent;
-    public static get ActiveComponent () {
-        return this.activeComponent;
-    }
-    public static set ActiveComponent (component) {
+export default class ControlComponent extends GroupComponent {
+    static activeComponent: ControlComponent;
+    static setActive(component: ControlComponent) {
         this.activeComponent = component;
     }
 
-    public static globalControls = new Map<string, ExecuteControl>();
-    public static isGlobal(control: string): boolean {
-        return this.globalControls.has(control);
+    constructor(width?: number, height?: number) {
+        super(width, height);
     }
 
-    public controls = new Map<string, ExecuteControl>();
-    public addControl(
-        sequence: string, 
-        execute: ExecuteControl, 
-        isGlobal: boolean = false
-    ): this {
-        if (ControlComponent.globalControls.get(sequence))
-        throw new ReferenceError('This sequience already is global control');
-
-        if (isGlobal) 
-        ControlComponent.globalControls.set(sequence, execute)
-        else 
-        this.controls.set(sequence, execute);
-
-        return this;
+    public controls: Map<string, Function> = new Map<string, Function>();
+    public addControl(controlSequence: string, execute: Function): this {
+        this.controls.set(controlSequence, execute);
+        return this
     }
 }
