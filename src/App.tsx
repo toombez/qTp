c22cb7f89f2ca13ad3bd378de2f08c3a3eb06fbf
Timur <81884993+toombez@users.noreply.github.com>
2023-02-05 02:28:29 +0300

    Refactor/run-by-scripts (#3)

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Box, Text } from "ink"
+import React, { FC } from "react"
+
+const App: FC<{}> = () => {
+    return (
+        <Box>
+            <Text>
+                Lorem, ipsum.
+            </Text>
+        </Box>
+    )
+}
+
+export default App
+export { App }
