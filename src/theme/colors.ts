7f63d76b0b35e60021ba1b894faf7fb335a6c3e9
Timur <timurtokaev.toombez@gmail.com>
2023-02-06 00:27:34 +0300

    Add theme colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+    cyan: '#177E89',
+    darkCyan: '#084C61',
+    red: '#E84855',
+    yellow: '#FFC857',
+    green: '#70AE6E',
+} as const
+
+export default colors
+export { colors }
