e4704d6b8af5f33184873e990b84d10948c4f0af
Timur <timurtokaev.toombez@gmail.com>
2023-02-08 20:57:05 +0300

    Add focus and blur handlers

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,32 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Box, BoxProps, useFocus, useInput } from "ink"
 
 interface IButtonProps extends BoxProps {
     children: React.ReactNode
     disabled?: boolean
     onClick?: () => void
+    onFocus?: () => void
+    onBlur?: () => void
 }
 
 const Button: React.FC<IButtonProps> = ({
     children,
     disabled = false,
     onClick,
+    onFocus,
+    onBlur,
     ...boxProps
 }) => {
     const { isFocused } = useFocus()
 
+    useEffect(() => {
+        if (isFocused && onFocus) {
+            onFocus()
+        }
+        if (!isFocused && onBlur) {
+            onBlur()
+        }
+    }, [isFocused])
     const canBeClicked = !disabled && isFocused
 
     useInput((_, key) => {
